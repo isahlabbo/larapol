52841139721d379326552619ee474da02ae87a77
Abu Saleem <abusaleem>
2019-05-18 22:14:35 +0100

    create models and extends model in the controller

---
+++ app/Http/Controllers/VoteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inani\Larapoll\Http\Controllers\VoteManagerController;
+
+class VoteController extends VoteManagerController
+{
+    //
+}
